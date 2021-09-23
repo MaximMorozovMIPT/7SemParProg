cf71709d81a398e9cf180452fd0f1ee82a0770be
Max <m.max.2000@mail.ru>
2021-09-23 15:35:18 +0300

    small code style fix

--- Task1/HelloWorld.c
+++ Task1/HelloWorld.c
@@ -4,17 +4,19 @@
 int main() 
 {
     int max_threads_id = omp_get_max_threads() - 1;
-    
+
     // Every thread waiting when it will be called to print it's id and break the loop
     #pragma omp parallel shared(max_threads_id)
-    while(1)
     {
-        int tid = omp_get_thread_num();
-        if(max_threads_id == omp_get_thread_num())
+        while(1)
         {
-            printf("Hello world from omp thread %d\n", tid);
-            --max_threads_id;
-            break;
+            int tid = omp_get_thread_num();
+            if(max_threads_id == omp_get_thread_num())
+            {
+                printf("Hello world from omp thread %d\n", tid);
+                --max_threads_id;
+                break;
+            }
         }
     }
 

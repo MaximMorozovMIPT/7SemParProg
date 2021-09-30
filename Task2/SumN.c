962d4c4ececd6cbcc9fafd539f0b6004be77c5b5
Max <m.max.2000@mail.ru>
2021-09-30 10:34:33 +0300

    Fix

--- Task2/SumN.c
+++ Task2/SumN.c
@@ -5,11 +5,14 @@
 int main(int argc, char* argv[]) 
 {
     printf("Num of threads = %d\n", omp_get_max_threads());
+    // Read launch argument
     int N = atoi(argv[1]);
     int sumN = 0;
 
+    // Parallel loop to sum numbers to N
+    // Using reduction to let every thread have his own sum and add them all at the end of the loop
     #pragma omp parallel for reduction(+: sumN) schedule(guided)
-    for(int i = 0; i < N; ++i)
+    for(int i = 1; i <= N; ++i)
     {
         sumN+=i;
     }

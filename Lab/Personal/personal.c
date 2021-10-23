1c645a67b82b7a250ab64c91417a3cf3187b51c0
Max <m.max.2000@mail.ru>
2021-10-23 10:57:37 +0300

    experiment

--- Lab/Personal/personal.c
+++ Lab/Personal/personal.c
@@ -16,8 +16,8 @@ int main(int argc, char **argv)
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     ierr = MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     
-    int rows_per_thread = ISIZE / world_size;
-    int rows_num_last = ISIZE - rows_per_thread * (world_size - 1);
+    int rows_per_thread = JSIZE / world_size;
+    int rows_num_last = JSIZE - rows_per_thread * (world_size - 1);
     
     int i, j;
 
@@ -27,49 +27,55 @@ int main(int argc, char **argv)
 
         double* a = (double*)calloc(ISIZE * JSIZE, sizeof(double));
 
-        int shift = rows_per_thread * JSIZE;
-        if(world_size > 1)
-        {   
-            for (int i = 1; i < world_size - 1; ++i) 
-            {
-                MPI_Recv(&a[shift*i], shift, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            }
-            
-            MPI_Recv(&a[shift*(world_size - 1)], rows_num_last * JSIZE, MPI_DOUBLE, world_size - 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+        int shift = rows_per_thread * ISIZE;
 
         // Count time of work algorithm
         struct timeval begin, lowmid, highmid, end;
         gettimeofday(&begin, 0);
-
-        for (i=0; i<rows_per_thread; i++)
+        if(world_size == 1)
         {
-            for (j=0; j<JSIZE; j++)
+            for (j=0; j<rows_per_thread; j++)
             {
-                a[i*ISIZE + j] = 10*i +j;
+                for (i=0; i<ISIZE; i++)
+                {
+                    a[j*ISIZE + i] = 10*i +j;
+                }
+            }
+
+            for (i = 8; i < ISIZE; i++)
+            {
+                for (j=0; j<rows_per_thread - 3; j++)
+                {
+                    a[j * ISIZE + i] = sin(0.00001*a[(j + 3)* ISIZE + i - 8]);
+                }
             }
         }
+        else
+        {
+            for (j=0; j<rows_per_thread + 6; j++)
+            {
+                for (i=0; i<ISIZE; i++)
+                {
+                    a[j*ISIZE + i] = 10*i +j;
+                }
+            }
 
-        gettimeofday(&lowmid, 0);
-
+            for (i = 8; i < ISIZE; i++)
+            {
+                for (j=0; j<rows_per_thread+3; j++)
+                {
+                    a[j * ISIZE + i] = sin(0.00001*a[(j + 3)* ISIZE + i - 8]);
+                }
+            }
+        }
         if(world_size > 1)
         {
-            for (int i = 1; i < world_size - 1; ++i) {
-                MPI_Send(&a[shift*i - 8 * JSIZE], shift + 8 * JSIZE, MPI_DOUBLE, i, SEND_TAG, MPI_COMM_WORLD);
-            }
-
-            int rows_num = ISIZE - rows_per_thread * (world_size - 1);
-            MPI_Send(&a[shift*(world_size - 1) - 8 * JSIZE], rows_num_last * JSIZE + 8 * JSIZE, MPI_DOUBLE, world_size - 1, SEND_TAG, MPI_COMM_WORLD);
-        }
-
-        gettimeofday(&highmid, 0);
-
-        for (i=8; i<rows_per_thread; i++)
-        {
-            for (j = 0; j < JSIZE-3; j++)
+            for (int i = 1; i < world_size - 1; ++i) 
             {
-                a[i * JSIZE + j] = sin(0.00001*a[(i-8) * JSIZE + j + 3]);
+                MPI_Recv(&a[shift*i], shift, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             }
+
+            MPI_Recv(&a[shift*(world_size - 1)], rows_num_last * JSIZE, MPI_DOUBLE, world_size - 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
         // Count time of work
@@ -78,15 +84,15 @@ int main(int argc, char **argv)
         long microseconds = end.tv_usec - begin.tv_usec;
         double elapsed = seconds + microseconds * 1e-6;
         printf("Elapsed time  %.10fs\n", elapsed);
+    
+        double* b = (double*)calloc(ISIZE * JSIZE, sizeof(double));
 
-        if(world_size > 1)
+        for(i = 0; i < ISIZE; ++i)
         {
-            for (int i = 1; i < world_size - 1; ++i) 
+            for(j = 0; j < JSIZE; ++j)
             {
-                MPI_Recv(&a[shift*i], shift, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                b[i*JSIZE + j]=a[j * ISIZE + i];
             }
-
-            MPI_Recv(&a[shift*(world_size - 1)], rows_num_last * JSIZE, MPI_DOUBLE, world_size - 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
         FILE *ff;
@@ -95,71 +101,61 @@ int main(int argc, char **argv)
         {
             for (j=0; j < JSIZE; j++)
             {
-                fprintf(ff,"%f ",a[i*ISIZE + j]);
+                fprintf(ff,"%f ",b[i*JSIZE + j]);
             }
-        fprintf(ff,"\n");
+            fprintf(ff,"\n");
         }
         fclose(ff);
         free(a);
+        free(b);
     }
     else if(world_rank != world_size - 1)
     {
-        double* a = (double*)calloc(rows_per_thread * JSIZE + 8 * JSIZE, sizeof(double));
+        double* a = (double*)calloc(rows_per_thread * ISIZE + 6 * ISIZE, sizeof(double));
         int shift = rows_per_thread * world_rank;
-
-        for (i=0; i<rows_per_thread; i++)
+        
+        for (j=0; j<rows_per_thread + 6; j++)
         {
-            for (j=0; j<JSIZE; j++)
+            for (i=0; i<ISIZE; i++)
             {
-                a[i*ISIZE + j] = 10*(i + shift) +j;
+                a[j*ISIZE + i] = 10*i + j + rows_per_thread * world_rank;
             }
         }
 
-        // Send the result of counting to thread 0
-        MPI_Send(a, rows_per_thread * JSIZE, MPI_DOUBLE, 0, SEND_TAG, MPI_COMM_WORLD);
-
-        MPI_Recv(a, rows_per_thread * JSIZE + 8 * JSIZE, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (i=8; i<rows_per_thread + 8; i++)
+        for (i = 8; i < ISIZE; i++)
         {
-            for (j = 0; j < JSIZE-3; j++)
+            for (j=0; j<rows_per_thread+3; j++)
             {
-                a[i * JSIZE + j] = sin(0.00001*a[(i-8) * JSIZE + j + 3]);
+                a[j * ISIZE + i] = sin(0.00001*a[(j + 3)* ISIZE + i - 8]);
             }
         }
 
-        MPI_Send(&a[8 * JSIZE], rows_per_thread * JSIZE, MPI_DOUBLE, 0, SEND_TAG, MPI_COMM_WORLD);
+        MPI_Send(a, rows_per_thread * ISIZE, MPI_DOUBLE, 0, SEND_TAG, MPI_COMM_WORLD);
         free(a);
     }
     else
     {
-        // Need to count last thread cause array size sometimes cannot be divided by number of threads
-        int rows_num = ISIZE - rows_per_thread * (world_size - 1);
-
         // Same as 1.
-        double* a = (double*)calloc(rows_num * JSIZE + 8 * JSIZE, sizeof(double));
+        double* a = (double*)calloc(rows_num_last * ISIZE, sizeof(double));
         int shift = rows_per_thread * world_rank;
 
-        // Same as 2.
-        for (i=0; i<rows_num; i++)
+        for (j=0; j<rows_num_last; j++)
         {
-            for (j=0; j<JSIZE; j++)
+            for (i=0; i<ISIZE; i++)
             {
-                a[i*ISIZE + j] = 10*(i + shift) +j;
+                a[j*ISIZE + i] = 10*i + j + rows_per_thread * world_rank;
             }
         }
-        
-        // Send the result of counting to thread 0
-        MPI_Send(a, rows_num_last * JSIZE, MPI_DOUBLE, 0, SEND_TAG, MPI_COMM_WORLD);
 
-        MPI_Recv(a, rows_num_last * JSIZE + 8 * JSIZE, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (i=8; i<rows_num_last + 8; i++)
+        for (i = 8; i < ISIZE; i++)
         {
-            for (j = 0; j < JSIZE-3; j++)
+            for (j=0; j<rows_num_last-3; j++)
             {
-                a[i * JSIZE + j] = sin(0.00001*a[(i-8) * JSIZE + j + 3]);
+                a[j * ISIZE + i] = sin(0.00001*a[(j + 3)* ISIZE + i - 8]);
             }
         }
-        MPI_Send(&a[8 * JSIZE], rows_num_last * JSIZE, MPI_DOUBLE, 0, SEND_TAG, MPI_COMM_WORLD);
+
+        MPI_Send(a, rows_num_last * ISIZE, MPI_DOUBLE, 0, SEND_TAG, MPI_COMM_WORLD);
         free(a);
     }
 
